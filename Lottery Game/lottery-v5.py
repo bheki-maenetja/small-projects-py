886d04141d509e0f96a78c1c34fcb99a03b6fae4
bheki-maenetja <bhekimaenetja@gmail.com>
2020-04-18 17:32:22 +0200

    complete lottery v5

--- Lottery Game/lottery-v5.py
+++ Lottery Game/lottery-v5.py
@@ -1,36 +1,34 @@
 from random import sample
 
 def main():
-  input("WELCOME TO THE LOTTERY!!! >>> ")
-
   total_winnings = 0
-  
+  input("WELCOME TO THE LOTTERY!!! >>> ")
   num_rounds = get_integer(1, 10, "How many rounds do you want to play?\nEnter a number between 1 and 10")
-  # print(num_rounds)
-  play_lotto()
+
+  for i in range(num_rounds):
+    input(f"Round {i+1} >>> ")
+    total_winnings += play_lotto()
+  
+  input(f"You've won a total of ${total_winnings}\nThank you for playing the Lottery!!! >>> ")
 
 def play_lotto():
-  # ticket = None
-  # draw = None
-  # winnings = 0
-  # num_matches = 0
-
-  game_settings = [
-    (10, '10-ball'),
-    (20, '20-ball'),
-    (50, '50-ball'),
-    (100, '100-ball'),
-    (1000, '1000-ball')
-  ]
+  game_settings = [(10, '10-ball'), (20, '20-ball'), (50, '50-ball'), (100, '100-ball'), (1000, '1000-ball')]
 
   print(f"\nGAME MODE\n{9*'-'}\n" + "\n".join([f"{game_settings.index(i) + 1}) {i[1]}" for i in game_settings]) + "\n")
   setting_index = get_integer(1, 5, "To set the game mode enter it's corresponding number")
   game_setting = game_settings[setting_index - 1]
-  
   input(f"You have chosen {game_setting[1]} >>> ")
+  
   ticket = get_ticket(game_setting[0])
   draw = sample(range(1, game_setting[0] + 1), 6)
-  print(f"Your ticket: {ticket}\nThe draw: {draw}")
+  matches = [i for i in ticket if i in ticket and i in draw]
+  winnings = calculate_winnings(len(matches), game_setting[0])
+
+  input(f"Your ticket: {ticket}\nThe draw: {draw} >>> ")
+  input(f"Your winning numbers: {matches} >>> ")
+  input(f"Congratulations you've won ${winnings} >>> ") if winnings > 0 else input("You've won nothing. Better luck next time >>> ")
+  
+  return winnings
 
 def get_ticket(num_range):
   input("Time to enter your lotto numbers >>> ")
@@ -45,6 +43,10 @@ def get_ticket(num_range):
       ticket.append(lotto_num)
   
   return ticket
+
+def calculate_winnings(num_hits, diff_setting):
+  prize_monies = (0, 10, 20, 50, 100, 500, 1000)
+  return prize_monies[num_hits] * diff_setting
 
 # Utitlity Functions
 def get_integer(min_value, max_value, input_prompt):
