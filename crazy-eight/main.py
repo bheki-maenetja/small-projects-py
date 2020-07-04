37f380966a3da00f47252c9f8be14e51ab0a7fcc
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-04 12:22:18 +0200

    more navigation tweaks

--- crazy-eight/main.py
+++ crazy-eight/main.py
@@ -27,21 +27,22 @@ def main(): # the main game function
       if player_choice == 'c':
         cards.view_cards(player_cards, 'Your cards')
         player_choice = util.get_string(1,1, 'Press b to go back or p to play a card', accept_values=['b', 'p'])
-        continue
       elif player_choice == 'p':
         player_card = cards.play_card(player_cards, current_card)
         if player_card != None:
           pile.append(player_card)
           current_card = pile[-1]
-          is_player_turn = not is_player_turn
+          is_player_turn = False
       elif player_choice == 't':
         print("You want to take a card")
-        is_player_turn = not is_player_turn
-      player_choice = util.get_string(1, 1, "Press t to take a card, c to view your your deck or p to play a card", ['c', 'p', 't'])
+        is_player_turn = False
+      else:
+        player_choice = util.get_string(1, 1, "Press t to take a card, c to view your your deck or p to play a card", ['c', 'p', 't'])
     else:
       input(f"There are now {len(pile)} in the pile >>> ")
       input("Its the computer's turn now! >>> ")
-      is_player_turn = not is_player_turn
+      is_player_turn = True
+      player_choice = ''
     # Update
     util.print_game_board(len(player_cards), len(comp_cards), current_card)
 
