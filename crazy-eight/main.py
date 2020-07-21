7772e64eff4bb034dc79b2bf04486d85cc9d09b8
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-21 14:34:48 +0200

    modified file imports

--- crazy-eight/main.py
+++ crazy-eight/main.py
@@ -1,14 +1,14 @@
 from random import shuffle, choice
 
-import cards
+from cards import * 
 import util
 
 # THE MAIN FUNCTION
 def main():
-  main_deck = cards.get_card_deck()
+  main_deck = get_card_deck()
   shuffled_deck = main_deck[:]
   shuffle(shuffled_deck)
-  player_cards, comp_cards, stack, pile = cards.assign_cards(shuffled_deck)
+  player_cards, comp_cards, stack, pile = assign_cards(shuffled_deck)
 
   current_card = pile[-1]
   current_suite = pile[-1].suite
@@ -16,16 +16,16 @@ def main():
   player_choice = ''
   attack_value = 0
   
-  cards.print_game_board(len(player_cards), len(comp_cards), current_card)
+  print_game_board(len(player_cards), len(comp_cards), current_card)
 
-  while not cards.is_game_over(player_cards, comp_cards, current_card):
+  while not is_game_over(player_cards, comp_cards, current_card):
     # Process Input (events)
     while is_player_turn:
       if player_choice == 'c':
-        cards.view_cards(player_cards, 'Your cards')
+        view_cards(player_cards, 'Your cards')
         player_choice = util.get_string(1,1, 'Press b to go back or p to play a card', accept_values=['b', 'p'])
       elif player_choice == 'p':
-        player_card = cards.play_card(player_cards, current_card, current_suite, attack_value > 0)
+        player_card = play_card(player_cards, current_card, current_suite, attack_value > 0)
         if player_card != None and player_card != 1:
           input(f"You have played the {player_card} >>> ")
           pile.append(player_card)
@@ -33,54 +33,55 @@ def main():
           current_suite = current_card.suite
           is_player_turn = True if current_card.value == 7 or current_card.value == 11 else False
           if current_card.value == 8:
-            current_suite = cards.change_suite()
+            current_suite = change_suite()
           elif current_card.value in [0, 2]:
             attack_value += current_card.power_value
         elif player_card == 1:
           input(f"The player will take {attack_value} cards >>> ")
-          cards.take_cards(attack_value, player_cards, stack)
-          cards.check_stack(stack, pile)
+          take_cards(attack_value, player_cards, stack)
+          check_stack(stack, pile)
           attack_value = 0
           is_player_turn = False
         player_choice = ''
       elif player_choice == 't':
-        cards.take_cards(1, player_cards, stack)
-        cards.check_stack(stack, pile)
+        take_cards(1, player_cards, stack)
+        check_stack(stack, pile)
         input(f"You have taken the {player_cards[-1]} >>> ")
         is_player_turn = False
         player_choice = ''
       else:
         player_choice = util.get_string(1, 1, "Press t to take a card, c to view your your deck or p to play a card", ['c', 'p', 't']) if attack_value == 0 else 'p'
     else:
-      cards.print_status(pile, stack, comp_cards)
-      input("Its the computer's turn now! >>> ")
-      comp_card = cards.comp_play_card(comp_cards, current_card, current_suite, attack_value > 0)
+      if not is_game_over(player_cards, comp_cards, current_card):
+        print_status(pile, stack, comp_cards)
+        input("Its the computer's turn now! >>> ")
+        comp_card = comp_play_card(comp_cards, current_card, current_suite, attack_value > 0)
 
-      if comp_card != None and comp_card != 1:
-        pile.append(comp_card)
-        input(f"The computer has played the {comp_card} >>> ")
-        current_card = pile[-1]
-        current_suite = current_card.suite
-        is_player_turn = False if current_card.value == 7 or current_card.value == 11 else True
-        if current_card.value == 8:
-          current_suite = cards.comp_change_suite(comp_cards)
-        elif current_card.value in [0, 2]:
-          attack_value += current_card.power_value
-      elif comp_card == 1:
-        input(f"The computer will take {attack_value} cards >>> ")
-        cards.take_cards(attack_value, comp_cards, stack)
-        cards.check_stack(stack, pile)
-        attack_value = 0
-        is_player_turn = True
-      else:
-        cards.take_cards(1, comp_cards, stack)
-        cards.check_stack(stack, pile)
-        input(f"The computer has taken the {comp_cards[-1]} >>> ")
-        is_player_turn = True
+        if comp_card != None and comp_card != 1:
+          pile.append(comp_card)
+          input(f"The computer has played the {comp_card} >>> ")
+          current_card = pile[-1]
+          current_suite = current_card.suite
+          is_player_turn = False if current_card.value == 7 or current_card.value == 11 else True
+          if current_card.value == 8:
+            current_suite = comp_change_suite(comp_cards)
+          elif current_card.value in [0, 2]:
+            attack_value += current_card.power_value
+        elif comp_card == 1:
+          input(f"The computer will take {attack_value} cards >>> ")
+          take_cards(attack_value, comp_cards, stack)
+          check_stack(stack, pile)
+          attack_value = 0
+          is_player_turn = True
+        else:
+          take_cards(1, comp_cards, stack)
+          check_stack(stack, pile)
+          input(f"The computer has taken the {comp_cards[-1]} >>> ")
+          is_player_turn = True
 
-      cards.print_status(pile, stack, comp_cards)
+      print_status(pile, stack, comp_cards)
     # Update
-    cards.print_game_board(len(player_cards), len(comp_cards), current_card)
+    print_game_board(len(player_cards), len(comp_cards), current_card)
 
   input("GAME OVER\nThank your for playing >>> ")
 
