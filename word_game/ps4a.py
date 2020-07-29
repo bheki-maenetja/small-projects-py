355e3fda5da5018d4d715f2469bd252a0b19c457
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-29 10:44:45 +0200

    completed playHand function

--- word_game/ps4a.py
+++ word_game/ps4a.py
@@ -183,7 +183,7 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
+    return sum(hand[i] for i in hand)
 
 
 
@@ -209,35 +209,39 @@ def playHand(hand, wordList, n):
       n: integer (HAND_SIZE; i.e., hand size required for additional points)
       
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Keep track of the total score
-    
+    score = 0
     # As long as there are still letters left in the hand:
+    while sum(hand[i] for i in hand) != 0:
     
         # Display the hand
-        
+        print("Current hand:", end=" ")
+        displayHand(hand)
         # Ask user for input
-        
+        player_word = input("Enter word, or a \".\" to indicate that you are finished: ")
         # If the input is a single period:
-        
+        if player_word == ".":
             # End the game (break out of the loop)
-
+            break
             
         # Otherwise (the input is not a single period):
-        
+        else:
             # If the word is not valid:
-            
+            if not isValidWord(player_word, hand, wordList):
                 # Reject invalid word (print a message followed by a blank line)
-
+                print("Invalid word, please try again.\n")
             # Otherwise (the word is valid):
-
+            else:
                 # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-                
+                word_score = getWordScore(player_word, n)
+                score += word_score
+                print("\"", player_word, "\"", "earned", word_score, "points. Total:", score, "points\n")
                 # Update the hand 
-                
+                hand = updateHand(hand, player_word)
+
 
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
-
+    print("Run out of letters. Total score:", score, "points")
 
 #
 # Problem #5: Playing a game
