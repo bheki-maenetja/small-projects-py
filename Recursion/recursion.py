d28bf74dde3eb7f33918cdff2d061d074b0246e1
bheki-maenetja <bhekimaenetja@gmail.com>
2020-09-02 11:39:16 +0200

    modified recursion directory

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -1,5 +1,6 @@
 import time
 
+# Factorial - A simple function to find n!
 def factorial(n):
   if n == 0:
       result = 1
@@ -7,51 +8,55 @@ def factorial(n):
       result = n * factorial(n-1)
   return result
 
+# Timer - A function to find the runtime of any function
 def blockFunc():
   start_time = time.time()
   factorial(996)
   end_time = time.time()
   print(f'Runtime: {(end_time - start_time) * 1000} ms')
 
-# blockFunc()
-
+# Count Down - Count down from n to 0
 def countDownFrom(n):
     print(n)
     if n > 0:
         countDownFrom(n-1)
 
-# countDownFrom(10)
-
+# Count Up - Count up from 0 to n
 def countUpTo(n):
     if n > 0:
         countUpTo(n-1)
     print(n)
 
+# Sum List - Finds the sum all elements in a list (assuming all elements are numeric)
 def sumNumList(arr, index = 0, num_sum = 0):
   if index == len(arr):
     return num_sum
   num_sum += arr[index]
   return sumNumList(arr, index + 1, num_sum)
 
+# Max Num - Finds the biggest number in a list
 def maxNum(arr, index = 0, max_num = 0):
   if index == len(arr): 
     return max_num
-  if arr[index] > max_num: max_num = arr[index]
+  if arr[index] > max_num: 
+    max_num = arr[index]
   index += 1
   return maxNum(arr, index, max_num)
 
+# Fibbo - Returns a list of n fibbonacci numbers
 def fibbo(n, arr = [1,1], i = 1):
-  if len(arr) == n or n <= 2: return arr[-1]
+  if len(arr) == n or n <= 2: 
+    return arr[-1]
   arr.append(arr[i] + arr[i-1])
   i += 1
   return fibbo(n, arr, i)
 
-def coin_flips(n, arr = []):
-  pass
-  
+# Last Character - Returns the last character in a string
 def lastChar(string, i = 0):
-  if string == '': return string
-  if i == len(string) - 1: return string[i]
+  if string == '': 
+    return string
+  if i == len(string) - 1: 
+    return string[i]
   return lastChar(string, i + 1)
 
 print(lastChar(''))
@@ -72,9 +77,3 @@ def isIn(char, aStr):
   elif char < aStr[len(aStr) // 2]:
     return isIn(char, aStr[:len(aStr) // 2])
 
-# print(fibbo(4))
-
-# print(maxNum([3,4,2,1,2]))
-
-# print(sumNumList([2,4,5,8]))
-# countUpTo(10)
