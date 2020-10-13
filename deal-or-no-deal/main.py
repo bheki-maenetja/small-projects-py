98dd743e7230b6441d71b03dce4b7b66945a11dd
bheki-maenetja <bhekimaenetja@gmail.com>
2020-10-13 19:16:31 +0100

    imported version function into main file

--- deal-or-no-deal/main.py
+++ deal-or-no-deal/main.py
@@ -1,10 +1,11 @@
 import v1.deal as version1
 import v2.deal_v2 as version2
+import v3.deal_v3 as version3
 
 def game_handler():
   input("Welcome to Deal or No Deal >>> ")
   input("You have chosen version 2 >>> ")
-  version2.start_game()
+  version3.play_game()
 
 if __name__ == "__main__":
     game_handler()
---
+++ deal-or-no-deal/v3/deal_v3.py
@@ -0,0 +1,2 @@
+def play_game():
+  input("You're playing the soon to be completed Deal or No Deal 3.0!!! >>> ")
