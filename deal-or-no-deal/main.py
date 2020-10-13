e851a2f74726b157974f13eda6f6efed056cb6ba
bheki-maenetja <bhekimaenetja@gmail.com>
2020-10-13 19:52:36 +0100

    implemented functionality for user to choose game version

--- deal-or-no-deal/main.py
+++ deal-or-no-deal/main.py
@@ -1,11 +1,19 @@
 import v1.deal as version1
 import v2.deal_v2 as version2
 import v3.deal_v3 as version3
+from util.util_functions import get_integer
 
 def game_handler():
   input("Welcome to Deal or No Deal >>> ")
-  input("You have chosen version 2 >>> ")
-  version3.play_game()
+  print("1. Version 1 (Original)", "2. Version 2 (Bonus Features)", "3. Version 3 (NEW STUFF)", sep="\n")
+  
+  version_choice = get_integer("To choose a version enter it's corresponding number", max_value=3, min_value=1, break_value='q')
+  if version_choice == 1:
+    version1.playGame()
+  elif version_choice == 2:
+    version2.start_game()
+  elif version_choice == 3:
+    version3.play_game()
 
 if __name__ == "__main__":
     game_handler()
