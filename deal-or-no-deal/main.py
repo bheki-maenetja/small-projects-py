72180c2f7fbe2474c240e73c1d834cbce3d60bb2
bheki-maenetja <bhekimaenetja@gmail.com>
2020-10-17 14:28:07 +0100

    touch ups

--- deal-or-no-deal/main.py
+++ deal-or-no-deal/main.py
@@ -4,16 +4,15 @@ import v3.deal_v3 as version3
 from util.util_functions import get_integer
 
 def game_handler():
-  print("Choose the version of the game you want to play\n")
-  print("1) Deal or No Deal (Original)", "2) Deal or No Deal 2 (Bonus Features)", "3) All New Deal or No Deal (NEW STUFF)", sep="\n")
-
-  version_choice = get_integer("\nTo choose a version enter it's corresponding number", max_value=3, min_value=1, break_value='q')
-  if version_choice == 1:
-    version1.playGame()
-  elif version_choice == 2:
-    version2.start_game()
-  elif version_choice == 3:
-    version3.play_game()
+    print("Choose the version of the game you want to play\n")
+    print("1) Deal or No Deal (Original)", "2) Deal or No Deal 2 (Bonus Features)", "3) All New Deal or No Deal (NEW STUFF)", sep="\n")
+    version_choice = get_integer("\nTo choose a version enter it's corresponding number", max_value=3, min_value=1, break_value='q')
+    if version_choice == 1:
+        version1.playGame()
+    elif version_choice == 2:
+        version2.start_game()
+    elif version_choice == 3:
+        version3.play_game()
 
 if __name__ == "__main__":
     game_handler()
