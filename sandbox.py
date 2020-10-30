01ec5ebd49b8560231428fbc6f37406212e4d629
bheki-maenetja <bhekimaenetja@gmail.com>
2020-10-30 10:27:03 +0000

    tkinter practice

--- sandbox.py
+++ sandbox.py
@@ -1,21 +1,4 @@
-def f(a, b):
-    return a + b
+import tkinter as tk
 
-def g(a, b):
-    return a > b
-
-def dict_interdiff(d1, d2):
-    '''
-    d1, d2: dicts whose keys and values are integers
-    Returns a tuple of dictionaries according to the instructions above
-    '''
-    # Your code here
-    d1_keys = set(d1.keys())
-    d2_keys = set(d2.keys())
-    intersect_keys = d1_keys.intersection(d2_keys)
-    diff_keys = d1_keys.symmetric_difference(d2_keys)
-    return (
-        { key: f(d1[key], d2[key]) for key in intersect_keys },
-        { key: (d1[key] if d1.get(key) != None else d2[key]) for key in diff_keys },
-    )
-
+window = tk.Tk()
+window.mainloop()
