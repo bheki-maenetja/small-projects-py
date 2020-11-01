e6fdfb2032fac7dc3c2fab0af136832826a2d430
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-01 18:00:15 +0000

    tkinter practice

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,26 @@
 import tkinter as tk
 
+my_text = "This is just some text"
+
 window = tk.Tk()
+
+greeting = tk.Label(text=f"Here is some text: {my_text}", fg="white", background="black")
+greeting.pack()
+
+button = tk.Button(
+    text="Don't Click Me/Click Me",
+    width=20,
+    height=5,
+    bg="blue",
+    fg="red",
+)
+button.pack()
+
+user_text = tk.Entry(
+    fg="yellow",
+    bg="blue",
+    width=50
+)
+user_text.pack()
+
 window.mainloop()
