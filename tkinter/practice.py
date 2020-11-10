46a5eb0597bf166c7c9a8d305cfa9d7f05757545
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-10 18:09:10 +0000

    new file

--- tkinter/practice.py
+++ tkinter/practice.py
@@ -79,21 +79,7 @@ frame_10 = tk.Frame(master=window_2, width=100, height=100, bg="pink")
 frame_10.grid(row=2, column=3)
 
 # grid()
-def make_square(x,y,colour="white"):
-    new_frame = tk.Frame(master=window_3, width=100, height=100, bg=colour)
-    new_frame.grid(row=x, column=y)
-    # label = tk.Label(master=new_frame, text=colour, fg="black", bg=colour)
-    # label.place(x=20, y=30)
 
-def make_board():
-    for i in range(8):
-        for j in range(8):
-            if (i + j) % 2 == 0:
-                make_square(i,j, colour="purple")
-            else:
-                make_square(i,j, colour="violet")
-
-make_board()
 # window.mainloop()
 # window_2.mainloop()
 window.destroy()
