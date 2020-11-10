79814bdd413095b576383788b8d9935de55bba9b
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-10 18:48:19 +0000

    chess board

--- tkinter/chess_board.py
+++ tkinter/chess_board.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 
-window = tk()
+window = tk.Tk()
 
 def make_square(x,y,colour="white"):
-    new_frame = tk.Frame(master=window_3, width=100, height=100, bg=colour)
+    new_frame = tk.Frame(master=window, width=100, height=100, bg=colour)
     new_frame.grid(row=x, column=y)
     # label = tk.Label(master=new_frame, text=colour, fg="black", bg=colour)
     # label.place(x=20, y=30)
@@ -12,8 +12,9 @@ def make_board():
     for i in range(8):
         for j in range(8):
             if (i + j) % 2 == 0:
-                make_square(i,j, colour="purple")
+                make_square(i,j, colour="cyan")
             else:
-                make_square(i,j, colour="violet")
+                make_square(i,j, colour="navy")
 
 make_board()
+window.mainloop()
