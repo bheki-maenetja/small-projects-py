2b94fd5ad3c69d115204742b8c68c3d30054cbad
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-11 20:24:27 +0000

    built basic number pad

--- tkinter/number_pad.py
+++ tkinter/number_pad.py
@@ -4,22 +4,27 @@ import tkinter.font as tkFont
 window = tk.Tk()
 window.resizable(False, False)
 
-top_frame = tk.Frame(height=100, width=100, borderwidth=5, relief=tk.RAISED)
+top_frame = tk.Frame(height=100, width=100)
 top_frame.pack(fill=tk.X, expand=1)
 
 heading_font = tkFont.Font(family="Courier", size=50)
-heading = tk.Label(master=top_frame, text="Number Pad", font=heading_font,fg="cyan", bg="navy")
+heading = tk.Label(master=top_frame, text="Number Pad", font=heading_font,fg="cyan", bg="navy", borderwidth=5, relief=tk.RAISED)
 heading.pack(fill=tk.X)
 
-label_font = tkFont.Font(family="Times", size=20)
-label_text = tk.Label(master=top_frame, text="123", font=label_font, fg="lime", bg="green")
-label_text.pack(fill=tk.X, side=tk.LEFT)
+label_font = tkFont.Font(family="Roboto", size=20)
+label_text = tk.Label(master=top_frame, text="", font=label_font, fg="yellow", borderwidth=2, relief=tk.GROOVE, bg="green", justify=tk.LEFT)
+label_text.pack(fill=tk.X)
+
+number_font = tkFont.Font(family="Verdana", size=30)
+
+def set_label(new_string):
+    label_text["text"] += new_string
 
 def create_button(digit):
-    new_button = tk.Button(master=body_frame, text=f"{digit}", height=3, width=15, fg="navy", bg="blue", command=lambda : print(digit))
+    new_button = tk.Button(master=body_frame, text=f"{digit}", height=2, width=5, fg="navy", bg="blue", font=number_font,command=lambda : set_label(f"{digit}"))
     return new_button
 
-body_frame = tk.Frame(height=100, width=100,bg="darkgreen", borderwidth=5, relief=tk.GROOVE)
+body_frame = tk.Frame(height=100, width=100,bg="purple", borderwidth=5, relief=tk.GROOVE)
 for i in range(3):
     body_frame.rowconfigure(i, weight=1, minsize=10)
     body_frame.columnconfigure(i, weight=1, minsize=10)
