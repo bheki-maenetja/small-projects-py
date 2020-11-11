26a14b279d0999fad2812b4665f9cad78d56b2d9
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-11 19:53:14 +0000

    number pad text

--- tkinter/number_pad.py
+++ tkinter/number_pad.py
@@ -4,12 +4,16 @@ import tkinter.font as tkFont
 window = tk.Tk()
 window.resizable(False, False)
 
-top_frame = tk.Frame(height=100, width=100,bg="navy", borderwidth=5, relief=tk.RAISED)
+top_frame = tk.Frame(height=100, width=100, borderwidth=5, relief=tk.RAISED)
 top_frame.pack(fill=tk.X, expand=1)
 
 heading_font = tkFont.Font(family="Courier", size=50)
 heading = tk.Label(master=top_frame, text="Number Pad", font=heading_font,fg="cyan", bg="navy")
-heading.pack()
+heading.pack(fill=tk.X)
+
+label_font = tkFont.Font(family="Times", size=20)
+label_text = tk.Label(master=top_frame, text="123", font=label_font, fg="lime", bg="green")
+label_text.pack(fill=tk.X, side=tk.LEFT)
 
 def create_button(digit):
     new_button = tk.Button(master=body_frame, text=f"{digit}", height=3, width=15, fg="navy", bg="blue", command=lambda : print(digit))
