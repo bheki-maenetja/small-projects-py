c78ae531c11907b17069321299515513a21cd163
bheki-maenetja <bhekimaenetja@gmail.com>
2020-11-12 20:18:19 +0000

    completed basic text editor

--- tkinter/text_editor.py
+++ tkinter/text_editor.py
@@ -1,17 +1,46 @@
 import tkinter as tk
 import tkinter.font as tkFont
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+# Event Handlers
+def open_file(): # open a file for editing
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete(1.0, tk.END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+def save_file(): # save the current file as a new file
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, tk.END)
+        output_file.write(text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+# Window Setup
 window = tk.Tk()
 window.title("Simple Text Editor")
 
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
+# Widgets
 txt_edit = tk.Text(master=window, relief=tk.SUNKEN, borderwidth=1)
 fr_buttons = tk.Frame(master=window, relief=tk.RAISED, borderwidth=2)
-btn_open = tk.Button(master=fr_buttons, text="Open")
-btn_save = tk.Button(master=fr_buttons, text="Save As")
+btn_open = tk.Button(master=fr_buttons, text="Open", command=open_file)
+btn_save = tk.Button(master=fr_buttons, text="Save As", command=save_file)
 
+# Layouting of Widgets
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
 
