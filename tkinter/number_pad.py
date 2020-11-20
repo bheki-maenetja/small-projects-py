1b2d1d8125169b4ae5f5043cc4f1d6ab318cb9db
Bheki Maenetja <bhekimaenetja@gmail.com>
2020-11-20 10:39:18 +0000

    Update number_pad.py
    trying to add functionality to number pad

--- tkinter/number_pad.py
+++ tkinter/number_pad.py
@@ -17,21 +17,29 @@ label_text.pack(fill=tk.X)
 
 number_font = tkFont.Font(family="Verdana", size=30)
 
-def set_label(new_string):
-    label_text["text"] += new_string
+def add_digit(new_string):
+    current_text = label_text["text"]
+    if current_text == "" or (current_text[0] != "0" and new_string != "0"):
+        label_text["text"] += new_string
     print(new_string)
 
-def create_button(digit):
-    new_button = tk.Button(master=body_frame, text=f"{digit}", height=1, width=5, fg="navy", bg="cyan", font=number_font,command=lambda : set_label(f"{digit}"))
+def create_button(digit, button_func):
+    new_button = tk.Button(master=body_frame, text=f"{digit}", height=1, width=5, fg="navy", bg="cyan", font=number_font,command=button_func)
     return new_button
+
+symbols = [".", 0, "<-"]
 
 body_frame = tk.Frame(height=100, width=100,bg="purple", borderwidth=5, relief=tk.GROOVE)
 for i in range(3):
     body_frame.rowconfigure(i, weight=1, minsize=10)
     body_frame.columnconfigure(i, weight=1, minsize=10)
-    for j in range(3):
-        new_button = create_button((i + 1) + j * 3)
-        new_button.grid(row=j, column=i, padx=5, pady=5, sticky="nesw")
+    for j in range(4):
+        if j == 3:
+            symbol_button = create_button(symbols[i], None)
+            symbol_button.grid(row=j, column=i, padx=5, pady=5, sticky="nesw")
+        else:
+            new_button = create_button((i + 1) + j * 3, lambda : add_digit(f"{(i + 1) + j * 3}"))
+            new_button.grid(row=j, column=i, padx=5, pady=5, sticky="nesw")
 
 body_frame.pack(fill=tk.BOTH, expand=1)
 window.mainloop()
