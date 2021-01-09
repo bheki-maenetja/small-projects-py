eeebfaee647858f0350954e3f2debcc2a4b20ccd
bheki-maenetja <bhekimaenetja@gmail.com>
2021-01-09 15:19:41 +0000

    touch ups

--- set-operations/sets.py
+++ set-operations/sets.py
@@ -18,3 +18,12 @@ def dict_interdiff(d1, d2):
         { key: f(d1[key], d2[key]) for key in intersect_keys },
         { key: (d1[key] if d1.get(key) != None else d2[key]) for key in diff_keys },
     )
+
+# from random import randint
+
+# my_list = [i for i in range(1, 102)]
+
+# list_subsets = (my_list[i:i+7] for i in range(0, len(my_list), 7))
+
+# for i, subset in enumerate(list_subsets):
+#     print(i, subset)
