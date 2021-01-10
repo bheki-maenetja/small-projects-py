ef6fe6ff10b6d0a8e38785635e856bd7b76818e4
bheki-maenetja <bhekimaenetja@gmail.com>
2021-01-10 21:54:07 +0000

    touch ups

--- set-operations/sets.py
+++ set-operations/sets.py
@@ -20,10 +20,7 @@ def dict_interdiff(d1, d2):
     )
 
 # from random import randint
-
 # my_list = [i for i in range(1, 102)]
-
 # list_subsets = (my_list[i:i+7] for i in range(0, len(my_list), 7))
-
 # for i, subset in enumerate(list_subsets):
 #     print(i, subset)
