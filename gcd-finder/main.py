d9f2d6cd3772b57f7fcf8f14ed66260b83e2131e
bheki-maenetja <bhekimaenetja@gmail.com>
2021-02-11 14:05:26 +0000

    playing around with euclids algorithm

--- gcd-finder/main.py
+++ gcd-finder/main.py
@@ -2,14 +2,14 @@
 
 # Iterative Solution
 def gcdIter(a,b):
-  test_value = min(a,b)
-  while not (a % test_value == 0 and b % test_value == 0):
-    test_value -= 1
-  return test_value
+    test_value = min(a,b)
+    while not (a % test_value == 0 and b % test_value == 0):
+        test_value -= 1
+    return test_value
 
 # Recursive Solution (Euclid's Algorithm)
 def gcdRecur(a,b):
-  if b == 0:
-    return a
-  else:
-    return gcdRecur(b, a % b)
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
