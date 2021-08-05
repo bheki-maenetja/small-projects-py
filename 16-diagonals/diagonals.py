7abdd92e332acce1cd69a30006cf49bcb2621abc
bheki-maenetja <bhekimaenetja@gmail.com>
2021-08-05 11:12:59 +0200

    figured out how to generate permutations of grids

--- 16-diagonals/diagonals.py
+++ 16-diagonals/diagonals.py
@@ -10,7 +10,7 @@ class BaseInt():
         assert 1 <= base <= 10
         self._base = base
     
-    def convert_num(self, num, partition=0):
+    def convert_num(self, num, partition=0, min_str_len=None):
         remainders = []
         div_mod = divmod(num, self._base)
         remainders.append(str(div_mod[1]))
@@ -21,7 +21,9 @@ class BaseInt():
         
         num_str = "".join(remainders)
 
-        if len(num_str) % partition != 0:
+        if min_str_len:
+            str_len = min_str_len
+        elif len(num_str) % partition != 0:
             str_len = (len(num_str) + partition) - (len(num_str) % partition)
         else:
             str_len = len(num_str)
@@ -33,9 +35,9 @@ class BaseInt():
         
         return num_str
     
-    def counter(self, n, partition=0):
+    def counter(self, n, partition=0, min_str_len=None):
         for i in range(n):
-            yield self.convert_num(i, partition)
+            yield self.convert_num(i, partition, min_str_len)
 
 class Board():
     def __init__(self, grid_size=5, num_diagonals=16) -> None:
@@ -49,21 +51,27 @@ class Board():
     def get_grid(self):
         return self._grid
     
-    def get_str_grid(self):
-        grid = [[str(i) for i in row] for row in self._grid]
+    def get_str_grid(self, grid):
+        grid = [[str(i) for i in row] for row in grid]
         grid_string = ""
         for row in grid:
             grid_string += " ".join(row) + "\n"
         return grid_string.strip("\n")
     
+    def convert_to_grid(self, bit_string):
+        return [
+            [int(i) for i in num_string] for num_string in bit_string.split(" ")
+        ]
+    
     def generate_grid_perms(self):
         n = 3 ** (self._grid_size ** 2)
         base3_int = BaseInt(3)
-        for i in base3_int.counter(n, 3):
-            print(i)
+        for num_str in base3_int.counter(n, self._grid_size, self._grid_size ** 2):
+            print("New Grid >>>>>")
+            print(self.get_str_grid(self.convert_to_grid(num_str)))
     
     def __repr__(self) -> str:
-        return self.get_str_grid()
+        return self.get_str_grid(self._grid)
 
 if __name__ == "__main__":
     new_board = Board(3)
