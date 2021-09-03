d651283b86d81f58cd8de31e3f9bfb1a843d40f0
bheki-maenetja <bhekimaenetja@gmail.com>
2021-09-03 16:14:27 +0200

    rethinking approach

--- 16-diagonals/diagonals.py
+++ 16-diagonals/diagonals.py
@@ -5,54 +5,6 @@ window = tk.Tk()
 window.aspect(1,1,1,1)
 window.minsize(300, 300)
 
-class BaseInt():
-    """
-    Class for representing integers in a given base
-    """
-    def __init__(self, base) -> None:
-        assert 1 <= base <= 10
-        self._base = base
-    
-    def convert_num(self, num, partition=0, min_str_len=None):
-        """
-        PARAMETERS
-            * num -> positive base 10 integer to be converted to the given base
-            * partition -> the size of each partition in the return string
-            * min_str_len -> the minimum length of the return string
-        RETURN VALUES
-            * num_str -> a bit string that represents num in the given base
-        WHAT DOES THIS FUNCTION DO?
-            * Converts a positive base 10 integer to the same number but in a 
-              different base
-        """
-        remainders = []
-        div_mod = divmod(num, self._base)
-        remainders.append(str(div_mod[1]))
-
-        while div_mod[0] != 0:
-            div_mod = divmod(div_mod[0], self._base)
-            remainders.insert(0, str(div_mod[1]))
-        
-        num_str = "".join(remainders)
-
-        if min_str_len:
-            str_len = min_str_len
-        elif len(num_str) % partition != 0:
-            str_len = (len(num_str) + partition) - (len(num_str) % partition)
-        else:
-            str_len = len(num_str)
-        
-        num_str = num_str.zfill(str_len)
-        num_str = " ".join(
-            [num_str[i:i+partition] for i in range(0, str_len, partition)]
-        )
-        
-        return num_str
-    
-    def counter(self, n, partition=0, min_str_len=None):
-        for i in range(n):
-            yield self.convert_num(i, partition, min_str_len)
-
 class Board():
     def __init__(self, grid_size=5, num_diagonals=16) -> None:
         self._window = tk.Tk()
@@ -104,9 +56,55 @@ class Board():
         for num_str in base_int.counter(n, self._grid_size, self._grid_size ** 2):
             yield self.str_to_grid(num_str)
     
+    def row_valid(self, row):
+        if len(row) == 1:
+            return True
+        
+        return not any(
+            (i + j) == 0 for i, j in zip(row[:-1], row[1:]) if i != 0 and j != 0
+        )
+    
+    def grid_valid(self, grid):
+        if len(grid) == 1:
+            return self.row_valid(grid[0])
+        
+        if not all(self.row_valid(row) for row in grid):
+            return False
+        
+        for row1, row2 in zip(grid[:-1], grid[1:]):
+            touching_diagonals = any(
+                abs(i + j) == 2 for i, j in zip(row1[1:], row2[:-1]) if i != 0 and j != 0
+            ) or any(
+                (i + j) == 2 for i, j in zip(row1[:-1], row2[1:]) if i != 0 and j != 0
+            )
+
+            if touching_diagonals:
+                return False
+        
+        return True
+
+    def place_diagonals(self, perm):
+        if len(perm) == self._grid_size and all(len(row) == 5 for row in perm):
+            print(self.grid_to_str(self._grid))
+            return
+        
+        for i in range(self._grid_size):
+            new_row = []
+            for i in range(-1, 2):
+                new_row.append(i)
+                print(new_row)
+                if not self.row_valid(new_row):
+                    new_row.pop()
+            perm.append(new_row)
+            if not self.grid_valid(perm):
+                perm.pop()
+        
+        print(perm)
+        # print(self.grid_to_str(perm))
+
     def __repr__(self) -> str:
         return self.grid_to_str(self._grid)
 
 if __name__ == "__main__":
-    new_board = Board(1)
-    new_board.generate_grid_perms()
+    new_board = Board(3)
+    new_board.place_diagonals([])
